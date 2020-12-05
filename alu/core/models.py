24a1ed06850c2301f178091cf5d60772192c89c8
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-12-05 22:35:18 +0000

    Activation model

--- alu/core/models.py
+++ alu/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+import django.utils.timezone as tz
+    
+class Activation(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True)
+    
+    def __str__(self):
+        return "{}".format(self.user.email)
