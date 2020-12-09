e8eec7194934b4a3cdb46143563104b048677f8a
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-12-09 14:28:08 +0000

    Move get_user_timeline out of TimelineManager and some cleanup

--- alu/profiles/models.py
+++ alu/profiles/models.py
@@ -9,33 +9,33 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-class TimelineManager(models.Manager):
-    def get_user_timeline(self, user, days=7):
-        # TODO: date format should be configurable
-        o = Timeline.objects.filter(user=user).earliest('timestamp').timestamp
-        oldest = date(o.year, o.month, o.day)
-        today = date.today()
-        delta = (today - oldest).days
-        if delta < days:
-            days = delta
-        first = today - timedelta(days=days)
-        dr = [d for d in (today - timedelta(days=n) for n in range(days+1))]
-        timeline = {d.strftime("%Y-%m-%d"): [] for d in dr}
-        for item in Timeline.objects.filter(user=user, timestamp__gte=first):
-            d = item.timestamp.strftime("%Y-%m-%d")
-            timeline[d].append(item.activity)
-        return timeline
-        
+    
 class Timeline(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE,
                              related_name='timeline')
     activity = models.TextField()
-
-    objects = TimelineManager()
     
     class Meta:
         ordering = ['-timestamp']
 
+    @property
+    def date(self):
+        ts = self.timestamp
+        return date(ts.year, ts.month, ts.day)
+
+def get_user_timeline(user, days=7):
+    # TODO: date format should be configurable
+    oldest = Timeline.objects.filter(user=user).earliest('timestamp').date
+    today = date.today()
+    delta = (today - oldest).days
+    if delta < days:
+        days = delta
+    first = today - timedelta(days=days)
+    dr = [d for d in (today - timedelta(days=n) for n in range(days+1))]
+    timeline = {d.strftime("%Y-%m-%d"): [] for d in dr}
+    for item in Timeline.objects.filter(user=user, timestamp__gte=first):
+        d = item.timestamp.strftime("%Y-%m-%d")
+        timeline[d].append(item.activity)
+    return timeline
--- alu/profiles/views.py
+++ alu/profiles/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Timeline
-
-#def home(request):
-#    if request.user.is_authenticated:
-#        return render(request, 'profiles/home.html', {
-#            'profile': request.user.profile,
-#            'timeline': Timeline.objects.get_user_timeline(request.user)
-#        })
-#    else:
-#        return redirect('index')
+from .models import Profile, Timeline, get_user_timeline
 
 @login_required
 def view_profile(request, uid):
@@ -18,5 +9,5 @@ def view_profile(request, uid):
     user = UserModel.objects.get(pk=uid)
     return render(request, 'profiles/profile.html', {
         'profile': user.profile,
-        'timeline': Timeline.objects.get_user_timeline(user)
+        'timeline': get_user_timeline(user)
     })
